ce66da6d74db6e8c992b07bcbf77435175700961
elster009 <tmdgus199901@gmail.com>
2024-10-22 17:20:48 +0900

    [momo-CSH] view 관련 기능 수정 detail : 1. 생성시 user_code와 연동 추가 2. StudyMapper.xml pk증가 기능 추가 3. StudyMapper.xml trim, if 추가

--- momo/src/main/java/ks52team01/config/WebConfig.java
+++ momo/src/main/java/ks52team01/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 		// 로그인 체크 인터셉터 등록
 		LoginCheckInterceptor loginCheckInterceptor = new LoginCheckInterceptor();
-		registry.addInterceptor(loginCheckInterceptor).addPathPatterns("/exam/**").addPathPatterns("/user/userMyPage").addPathPatterns("/score/**").addPathPatterns("/user/userInfoModfiy").excludePathPatterns("/exam/examMain");
+		registry.addInterceptor(loginCheckInterceptor).addPathPatterns("/exam/**").addPathPatterns("/user/userMyPage").addPathPatterns("/score/**").addPathPatterns("/user/userInfoModfiy").addPathPatterns("/study/**").excludePathPatterns("/exam/examMain");
 
 	}
 
