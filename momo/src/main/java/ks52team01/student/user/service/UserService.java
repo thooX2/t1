e4696ec9cd762b5758268877137fa44a6e74081b
LeeSoYoung <lsy_0118@naver.com>
2024-10-23 17:04:15 +0900

    [momo-LSY] 회원가입 유효성 체크 코드 작성 detail: 이후 insert 쿼리문 작성

--- momo/src/main/java/ks52team01/student/user/service/UserService.java
+++ momo/src/main/java/ks52team01/student/user/service/UserService.java
@@ -1,15 +1,13 @@
 package ks52team01.student.user.service;
 
+import ks52team01.common.ResponseError;
 import ks52team01.student.user.dto.User;
+import ks52team01.student.user.dto.UserJoinInfo;
 
 public interface UserService {
 	
 
 	public void userMyPage() ;
-	
-	public void loginAction();
-	
-	public void logoutAction();
 	
 	public void userInfoModify();
 	
@@ -28,5 +26,7 @@ public interface UserService {
 	public User getUserById(User user);
 
 	public int userLoginAction(User user);
+
+	public ResponseError userJoinCheck(UserJoinInfo userJoinData);
 	
 }
