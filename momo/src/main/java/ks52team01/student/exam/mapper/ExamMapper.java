c52c87ac8bf6f0c503d34052a9b2bf8d63b67090
Doyoung9568 <ehdud9367@gmail.com>
2024-11-01 16:50:42 +0900

    [MOMO-KDY]exam 분석 부분 수정 및 삭제 1.ExamAnalyseDto 추가 2.student-exam 패키지부분 일부 수정 및 추가 3.css파일 수정으로 인한 파일명 변경(smartmath->customsmartmath) 4.view-user-exam-analyse관련 html 일부분 수정

--- momo/src/main/java/ks52team01/student/exam/mapper/ExamMapper.java
+++ momo/src/main/java/ks52team01/student/exam/mapper/ExamMapper.java
@@ -3,12 +3,15 @@ package ks52team01.student.exam.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import ks52team01.student.exam.dto.ExamAnalyseDto;
 import ks52team01.student.exam.dto.TookExamInfo;
 
 @Mapper
 public interface ExamMapper {
-
+	//
+	 List<ExamAnalyseDto> selectUserExamData(@Param("userCode") String userCode, @Param("majorCode") String majorCode);
 	// 회원이 응시한 모의고사 목록 조회
 	List<TookExamInfo> getTookExamList(String userCode);
 }
