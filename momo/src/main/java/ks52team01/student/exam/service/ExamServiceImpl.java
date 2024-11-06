df65151bb94e8f12f4eebc5d1fc10db4cb83ca1b
devil7861 <munsm2036@gmail.com>
2024-11-06 16:49:45 +0900

    [momo-MSM] user페이지 exam파트 detail : 1. 모의고사 목록(user_exam_list)에서    문제를 등록한 모의고사만 나오게 처리

--- momo/src/main/java/ks52team01/student/exam/service/ExamServiceImpl.java
+++ momo/src/main/java/ks52team01/student/exam/service/ExamServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ks52team01.student.exam.dto.ExamAnalyse;
+import ks52team01.student.exam.dto.ExamInfo;
 import ks52team01.student.exam.mapper.ExamMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,11 +17,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ExamServiceImpl implements ExamService {
 
-    private final ExamMapper examMapper;
-    
+	private final ExamMapper examMapper;
 
-   @Override
-    public List<ExamAnalyse> getUserExamData(String userCode, String majorCode) {
-        return examMapper.selectUserExamData(userCode, majorCode);
-    }
+	@Override
+	public List<ExamInfo> getRegisteredQustionExamList() {
+
+		return examMapper.getRegisteredQustionExamList();
+	}
+
+	@Override
+	public List<ExamAnalyse> getUserExamData(String userCode, String majorCode) {
+		return examMapper.selectUserExamData(userCode, majorCode);
+	}
 }
