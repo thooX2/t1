d21a3712306c7b71e91c998fca999e9481e055a9
Kim Seung Soo <thox2q9@gmail.com>
2024-11-15 13:40:59 +0900

    [momo-KSS] 회원관리 수정

--- momo/src/main/java/ks52team01/admin/user/mapper/AdminMapper.java
+++ momo/src/main/java/ks52team01/admin/user/mapper/AdminMapper.java
@@ -7,23 +7,5 @@ import ks52team01.student.user.dto.User;
 @Mapper
 public interface AdminMapper {
 
-	public void adminPage();
-
-	public void adminLoginMove();
-
-	public void adminLogoutAction();
-
-	public void adminInfoModifyAction();
-
-	public void adminJoinAction();
-
-	public void adminInfoDetailAction();
-
-	public void adminDeleteAction();
-
-	public void adminFindIdAction();
-
-	public void adminFindPwAction();
-
 	public User getAdminById(String userId, String userPw);
 }
--- momo/src/main/java/ks52team01/admin/user/service/AdminService.java
+++ momo/src/main/java/ks52team01/admin/user/service/AdminService.java
@@ -1,24 +1,6 @@
 package ks52team01.admin.user.service;
 
 public interface AdminService {
-
-	public void adminPage();
 	
-	public void adminLoginMove() ;
-	
-	public void adminLogoutAction();
-	
-	public void adminInfoModifyAction();
-	
-	public void adminJoinAction();
-	
-	public void adminInfoDetailAction();
-	
-	public void adminDeleteAction();
-	
-	public void adminFindIdAction();
-	
-	public void adminFindPwAction();
-
 	public int adminLoginAction(String userId, String userPw);
 }
